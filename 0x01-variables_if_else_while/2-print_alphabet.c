0c6592275396cf342a9a286d78cbd201e5179573
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-09-11 15:12:51 +0100

    My commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+/**
+*main - entry
+*
+*Return: 0
+*/
+
+
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+    {
+	putchar(letter);
+	letter++;
+    }
+
+	putchar('\n');
+
+	return (0);
+}

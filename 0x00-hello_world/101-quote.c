f6954651177cde1d55607960830d5669e8289c9e
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-09-11 12:17:01 +0100

    My commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,17 @@
 #include <unistd.h>
 
+/**
+ *main - entry
+ *
+ *Return: 0
+ */
+
+
+
 int main(void)
 {
 	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = 57; // Length of the quote
+	ssize_t len = 60;
 
 	write(2, quote, len);
 

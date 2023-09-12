d1172b4aa114b2892417951538485c3b47f772b1
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-09-12 19:39:22 +0100

    My commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - entry
+ *
+ *Return: 0
+ */
+
+
+int main(void)
+{
+	char letter = 'a';
+
+
+	while (letter<= 'z')
+{
+	if (letter != 'q' && letter != 'e')
+{
+	putchar(letter);
+
+}
+	letter++;
+
+}
+
+	putchar('\n');
+
+	return 0;
+}

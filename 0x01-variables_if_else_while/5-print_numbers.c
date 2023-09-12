b3584f0020c7777351d9e5777728ae19bb47c6c5
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-09-12 19:59:34 +0100

    My commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - entry
+ *
+ *Return: 0
+ */
+
+
+int main(void)
+{
+	char number = '0';
+
+	while (number <= '9')
+{
+	putchar(number);
+	number++;
+	}
+	putchar('\n');
+	return (0);
+}

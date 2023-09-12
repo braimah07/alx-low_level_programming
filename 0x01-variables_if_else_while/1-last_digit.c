888080f011bf7a54fbfad88b188bf9967e316edb
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-09-12 16:33:53 +0100

    My commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry
+ *
+ *Return: 0
+ */
+
 
 int main(void)
+
 {
-    char letter = 'a';
+	int n;
 
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
+	srand(time(0)); /* Seed the random number generator with the current time */
 
-    putchar('\n');
+	n = rand(); /* Generate a random number */
 
-    return 0;
+	printf("Last digit of %d is %d", n, n % 10);
+
+	if (n % 10 > 5)
+	{
+	printf(" and is greater than 5\n");
+	}
+	else if (n % 10 == 0)
+	{
+	printf(" and is 0\n");
+	}
+	else
+	{
+	printf(" and is less than 6 and not 0\n");
+	}
+
+	return (0);
 }

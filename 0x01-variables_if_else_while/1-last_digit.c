db0baa929d5a67819d7816bec6bd3353e14ae536
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-09-13 22:55:07 +0100

    My commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,37 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- *main - entry
+ * main - Entry point
  *
- *Return: 0
+ * Return: Always 0 (Success)
  */
-
-
 int main(void)
-
 {
 	int n;
 
-	srand(time(0)); /* Seed the random number generator with the current time */
-
-	n = rand(); /* Generate a random number */
-
-	printf("Last digit of %d is %d", n, n % 10);
-
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n % 10 > 5)
 	{
-	printf(" and is greater than 5\n");
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if (n % 10 == 0)
 	{
-	printf(" and is 0\n");
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	else
 	{
-	printf(" and is less than 6 and not 0\n");
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-
 	return (0);
 }
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- *main - entry
+ * main - Entry point
  *
- *Return: 0
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-	int num1, num2;
+	int p, q;
 
-	for (num1 = 0; num1 <= 99; num1++)
+	for (p = 0; p <= 98; p++)
 	{
-	for (num2 = num1; num2 <= 99; num2++)
-	{
-	putchar((num1 / 10) + '0');
-	putchar((num1 % 10) + '0');
+	for (q = p + 1; q <= 99; q++)
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
 	putchar(' ');
-	putchar((num2 / 10) + '0');
-	putchar((num2 % 10) + '0');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
 
-	if (num1 != 99 || num2 != 99)
-	{
+	if (p == 98 && q == 99)
+	continue;
+
 	putchar(',');
 	putchar(' ');
 	}
 	}
-	}
-
 	putchar('\n');
-
 	return (0);
 }

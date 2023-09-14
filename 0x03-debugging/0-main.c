b56c9e5628bd6fdbb3f5eb79c234af1b7b30593f
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-09-14 23:43:11 +0100

    My commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
-void positive_or_negative(int i)
+/**
+ * main - tests function that prints if integer is positive, negative, or zero
+ * Return: 0
+ */
+
+int main(void)
 {
-    if (i > 0)
-    {
-        printf("%d is positive\n", i);
-    }
-    else if (i < 0)
-    {
-        printf("%d is negative\n", i);
-    }
-    else
-    {
-        printf("%d is zero\n", i);
-    }
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 #include "main.h"
-#define leapDaysTot 366
-#define daysTot 365
-#define janDaysTot 31
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into consideration
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
- */
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((!(year % 100 == 0) || year % 400 == 0)
-	&& year % 4 == 0)
-	{
-	if (month > 2 && day >= 60)
-	day++;
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", leapDaysTot - day);
-	}
-	else
-	{
-	if (month == 2 && day == 60)
-	printf("Invalid date: %02d/%02d/%04d\n",
-	month, day - janDaysTot, year);
-	else
-	{
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", daysTot - day);
-	}
-	}
+    int leap = 0;
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        leap = 1;
+    }
+
+    if ((month > 2 && leap) || (month >= 3 && leap))
+    {
+        day++;
+    }
+
+    if (month == 2 && day == 60 && !leap)
+    {
+        printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+    }
+    else
+    {
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", leap ? 366 - day : 365 - day);
+    }
 }

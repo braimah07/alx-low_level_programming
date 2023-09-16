48c600ef25de77aaf230a8033c056a49da782593
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-09-16 19:24:44 +0100

    My commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: 0 (Success)
+ *  * main - prints putchar
+ *   *
+ *    * Return: always 0
  */
+
 int main(void)
 {
-	printf('_putchar')
-	return (0);
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }

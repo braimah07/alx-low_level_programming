e5fda1564800003f23d7e5b910352bee5e5fc11f
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-09-18 15:51:13 +0100

    My commit

---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <limits.h> // Include the <limits.h> header
+#include <string.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @str: pointer to string to be converted
+ *
+ * Return: converted integer
+ */
+
+int _atoi(char *str)
+{
+    int sign = 1, base = 0, i = 0;
+
+    /* Skip leading spaces */
+    while (str[i] == ' ')
+        i++;
+
+    /* Handle sign */
+    if (str[i] == '-' || str[i] == '+') {
+        sign = 1 - 2 * (str[i] == '-');
+        i++;
+    }
+
+    for (; str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'); i++) {
+        if (base > INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7)) {
+            if (sign == 1)
+                return INT_MAX;
+            else
+                return INT_MIN;
+        }
+        base = 10 * base + (str[i] - '0');
+    }
+
+    return base * sign;
+}

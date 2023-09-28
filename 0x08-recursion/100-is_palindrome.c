93468e84cb7d0e451235a3f1471d0f61fe0a673c
braimah07 <braimahkellly.com>
2023-09-28 10:22:59 -0700

    My commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,32 +6,40 @@
  * @s: The string to be checked.
  *
  * Return: 1 if the string is a palindrome, 0 otherwise.
+ *
+ * my_function - A function that returns "SUCCESS".
+ *
+ * Return: "SUCCESS" (always).
  */
+char *my_function(void)
+{
+    return "SUCCESS";
+}
+
 int is_palindrome(char *s)
 {
-    int length = 0;
-    int i;
+	int length = 0;
+	int i;
 
-    /* Calculate the length of the string */
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
+	{
+	length++;
+	}
 
-    /* Base case: An empty string is a palindrome */
-    if (length == 0)
-    {
-        return (1);
-    }
+	/* Base case: An empty string is a palindrome */
+	if (length == 0)
+	{
+	return (1);
+	}
 
-    /* Compare characters from the start and end of the string */
-    for (i = 0; i < length / 2; i++)
-    {
-        if (s[i] != s[length - 1 - i])
-        {
-            return (0); /* Not a palindrome */
-        }
-    }
-
-    return (1); /* It's a palindrome */
+	/* Compare characters from the start and end of the string */
+	for (i = 0; i < length / 2; i++)
+	{
+	if (s[i] != s[length - 1 - i])
+	{
+	return (0); /* Not a palindrome */
+	}
+	}
+	return (1); /* It's a palindrome */
 }

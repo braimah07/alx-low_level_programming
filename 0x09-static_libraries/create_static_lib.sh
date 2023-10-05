ccf2f07a15a90b214e54fc397704d3ef48c1b75b
braimah07 <braimahkellly.com>
2023-10-05 11:05:06 -0700

    My commit

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Compile each .c file to a .o object file
-for file in *.c; do
-    gcc -Wall -Werror -Wextra -pedantic -c "$file"
-done
-
-# Create the static library liball.a
-ar rcs liball.a *.o
-
-# Clean up the .o object files
-rm -f *.o
+gcc -c *.c
+ar -rcs liball.a *.o

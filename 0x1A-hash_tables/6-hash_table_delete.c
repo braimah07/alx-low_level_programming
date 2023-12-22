9d343c65e8b7874f6a0f6e546775c12407ca12a4
kelly braimah <braimahkellly@gmail.com.com>
2023-12-22 20:38:47 +0100

    My commit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,30 +1,41 @@
-#include "hash_tables.h"
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
 
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: The hash table to be deleted.
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * @value: The value corresponding to the key
+ * @next: A pointer to the next node in the list
  */
-void hash_table_delete(hash_table_t *ht)
+typedef struct hash_node_s
 {
-    unsigned long int i;
-    hash_node_t *node, *temp;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
+} hash_node_t;
 
-    if (ht == NULL)
-        return;
+/**
+ * struct hash_table_s - Hash table
+ *
+ * @size: The size of the array
+ * @array: An array of size @size of pointers to the first node of a linked list
+ */
+typedef struct hash_table_s
+{
+    unsigned long int size;
+    hash_node_t **array;
+} hash_table_t;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            temp = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = temp;
-        }
-    }
+/* Function prototypes */
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
-    free(ht->array);
-    free(ht);
-}
+#endif /* HASH_TABLES_H */
